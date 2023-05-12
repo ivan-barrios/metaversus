da3d4494dba2278127aad261f133bd03a243bed1
Ivan Barrios <ivanbar8112003@gmail.com>
2023-05-12 20:57:05 -0300

    4th try for deployment

--- sections/About.jsx
+++ sections/About.jsx
@@ -13,7 +13,7 @@ const About = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title="| About Metaversus" textStyles="text-center" />
       <motion.p
